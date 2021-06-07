bdeacba66eb58b34dc228ccc160b516aa5b15c7a
Deepak Kumar <deepak.2002.02.16@gmail.com>
2021-06-08 04:02:04 +0530

    space clearing

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 function addClass() {
   document.body.classList.add("sent");
 }
-
 sendLetter.addEventListener("click", addClass);
-
-document.getElementById("sendLetter").addEventListener("click", function(event){
+document.getElementById("sendLetter").addEventListener("click", function (event) {
   event.preventDefault()
 });
-
-
-
 const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll(".nav-list ul li");
 window.addEventListener("scroll", () => {
   let current = "";
-  // console.log(pageYOffset);
   sections.forEach((section) => {
     const sectionTop = section.offsetTop;
     const sectionHeight = section.clientHeight;
@@ -22,7 +16,6 @@ window.addEventListener("scroll", () => {
       current = section.getAttribute("id");
     }
   });
-
   navLi.forEach((li) => {
     li.classList.remove("active");
     if (li.classList.contains(current)) {
@@ -30,11 +23,10 @@ window.addEventListener("scroll", () => {
     }
   });
 });
-
 const menuBtn = document.querySelector('.menu-toggle');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
-  if(!menuOpen) {
+  if (!menuOpen) {
     menuBtn.classList.add('open');
     menuOpen = true;
   } else {
